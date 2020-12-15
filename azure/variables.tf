c301b280fe63773aa377a7e2c240883c437b75b2
Richard Shade <rshade@flexera.com>
2020-12-15 09:50:25 -0600

    Updating for azure

---
+++ azure/variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+
+variable "execution" {
+  type = string
+  description = "self service execution id"
+}
