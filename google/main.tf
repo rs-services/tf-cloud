43abdd59e4d00d2aecdae7709e00895084cef8c9
Richard Shade <rshade@flexera.com>
2020-12-22 08:11:09 -0600

    updating

--- google/main.tf
+++ google/main.tf
@@ -1,72 +1,36 @@
-variable "region" {
-  default = "us-west1"
-}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
-variable "zone" {
-  default = "us-west1-b"
-}
+  tags = ["foo", "bar"]
 
-variable "network_name" {
-  default = "tf-gke-k8s"
-}
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
-provider "google" {
-  region = var.region
-}
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
 
-resource "google_compute_network" "default" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
+  network_interface {
+    network = "default"
 
-resource "google_compute_subnetwork" "default" {
-  name                     = var.network_name
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = google_compute_network.default.self_link
-  region                   = var.region
-  private_ip_google_access = true
-}
-data "google_client_config" "current" {}
-data "google_container_engine_versions" "default" {
-}
+    access_config {
+      // Ephemeral IP
+    }
+  }
 
-resource "google_container_cluster" "default" {
-  name               = var.network_name
-  initial_node_count = 3
-  min_master_version = data.google_container_engine_versions.default.latest_master_version
-  network            = google_compute_subnetwork.default.name
-  subnetwork         = google_compute_subnetwork.default.name
+  metadata = {
+    foo = "bar"
+  }
 
-  // Use legacy ABAC until these issues are resolved: 
-  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
-  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
-  enable_legacy_abac = true
+  metadata_startup_script = "echo hi > /test.txt"
 
-  // Wait for the GCE LB controller to cleanup the resources.
-  // Wait for the GCE LB controller to cleanup the resources.
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sleep 90"
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
-
-output "network" {
-  value = google_compute_subnetwork.default.network
-}
-
-output "subnetwork_name" {
-  value = google_compute_subnetwork.default.name
-}
-
-output "cluster_name" {
-  value = google_container_cluster.default.name
-}
-
-output "cluster_region" {
-  value = var.region
-}
-
-output "cluster_zone" {
-  value = google_container_cluster.default.zone
-}
-
